dd95bd235cf4aed900a3971dbae1f614cd396a2f
COOLICEBLAST <cooliceblast0904@gmail.com>
2024-02-01 17:17:50 +0100

    truffle

---
+++ contracts/hello.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string myName = "Tristan";
+
+    function getMyName() public view  returns (string memory){
+        return myName;
+    }
+
+    function changeMyName(string memory _newName) public {
+        myName = _newName;
+    }
+}
